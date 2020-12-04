58b9f13049feeafe2e1edc4cc6441287f40bd787
Muzammil Tariq <muzammil413tariq@gmail.com>
2020-12-04 23:16:07 +0500

    setup production webpack

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import AppRouter from './Router/AppRouter';
 import { Provider } from 'react-redux';
 import ConfigureStore from './store/configureStore';
+import 'normalize-css/normalize.css';
+import "./styles/styles.scss";
+import 'react-dates/lib/css/_datepicker.css';
 const store = ConfigureStore();
+console.log('fg');
 ReactDOM.render(
     <Provider store={store}>
         <AppRouter />
